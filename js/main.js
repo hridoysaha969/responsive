c8ef30906a4d1cf8413b87f81c0142d6be4dca6b
hridoysaha969 <hridoysaha969@gmail.com>
2020-11-18 21:59:18 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+    });
+});
+
+var hamberger = document.querySelector('.hamberger');
+var times = document.querySelector('.times');
+var mobileNav = document.querySelector('.mobile-nav');
+var sideMenu = document.querySelector('.side-menu');
+
+hamberger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+});
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+sideMenu.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
